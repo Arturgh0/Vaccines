00c356ebfabd84bafa377ef4a5d79797a62eff23
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2021-03-24 19:12:06 -0300

    Update get_ids_from_twitter_pro_en.py

--- Code/Data_acquisition_and_understanding/get_ids_from_twitter_pro_en.py
+++ Code/Data_acquisition_and_understanding/get_ids_from_twitter_pro_en.py
@@ -6,7 +6,6 @@ c.Custom["tweet"] = ["id"]
 c.Search = "#ProVaccine OR #VaccinesSaveLives OR #ProVaxxer OR #ProVax, ProVaxx OR #VaccinesWork OR #VaccineForAll OR #VaccinesForAll OR #GetVaccinated OR #NOTanantivaxxer OR #IWillGetVaccinated OR #VaccinesAreSafe OR #VaccinesSave OR #VaccineSave OR #VaccinesNow OR #VaccinesAreGood OR #TrustVaccines OR #ITrustVaccines OR #IGotTheShot OR #IGotVaccinated OR #Vaccinated OR #VaccinationNow OR #VaccineWork OR #VaccineSaveLives OR #VaccinesAreGreat"
 c.Since = "2019-03-01"
 c.until = "2021-03-01"
-c.Lang = "pt"
 c.Output = "../../Data/Raw/ids_pro_en.csv"
 c.Store_csv = True
 
